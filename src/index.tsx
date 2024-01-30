c0e1e97f9e15b47d9178dc05ab18f0f8387c56ba
Brunno Dias <brunnorpdias@gmail.com>
2024-01-30 01:59:10 -0300

    Form fixed, display page created

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
-import { useState } from "react";
+import { Form, ActionPanel, Action, useNavigation, showToast } from "@raycast/api";
+import Answer from './answer';
 
 type Values = {
   prompt: string;
-  llm: string;
+  // llm: string;
   model: string;
-  instructions: string;
+  // instructions: string;
+  temperature: string;
+  streaming: boolean;
+};
+
+type ParsedValues = {
+  prompt: string;
+  // llm: string;
+  model: string;
+  // instructions: string;
   temperature: number;
   streaming: boolean;
 };
 
 export default function Command() {
+  const { push } = useNavigation();
+
   function handleSubmit(values: Values) {
-    console.log(values);
+    const parsedValues:ParsedValues = {
+      ...values,
+      temperature: parseFloat(values.temperature) 
+    }
+    console.log(parsedValues)
     showToast({ title: "Submitted form", message: "See logs for submitted values" });
+    push(<Answer data={parsedValues} />)
   }
 
   // add icons to the llms
@@ -25,15 +41,17 @@ export default function Command() {
   //   <Form.Dropdown.Item value="perplexity" title="Perplexity (offline)" />
   //   <Form.Dropdown.Item value="anthropic" title="Anthropic (offline)" />
   // </Form.Dropdown>
+  
+  // <Action title="Push" onAction={() => handleSubmit(values)} />
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} />
+          <Action.SubmitForm title="Submit" onSubmit={handleSubmit} />
         </ActionPanel>
       }
-      enableDrafts={true}
+      // enableDrafts={true}
     >
       <Form.TextArea id="prompt" title="Prompt" placeholder="Describe your request here" />
       <Form.Dropdown id="model" title="Model" defaultValue="gpt-3.5-turbo-1106">
@@ -46,10 +64,7 @@ export default function Command() {
         <Form.Dropdown.Item value="pplx-70b-online" title="Perplexity (off)" keywords={["online", "perplexity"]} />
       </Form.Dropdown>
       <Form.Separator />
-      <Form.TagPicker id="type" title="Response Type">
-        <Form.TagPicker.Item value="answer using bullet points" title="list" />
-      </Form.TagPicker>
-      <Form.TextField id="temperature" placeholder="0.7" />
+      <Form.TextField id="temperature" defaultValue="0.7" placeholder="0.7" />
       <Form.Checkbox id="stream" title="Streaming" label="Streaming or static response" defaultValue={true} />
     </Form>
   );
