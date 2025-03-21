27922af9f0e685548dbfc6837edeb281363123bf
Brunno Dias <brunnorpdias@gmail.com>
2025-03-21 17:51:25 +0000

    Remove all untracked files from gitignore

--- node_modules/@mistralai/mistralai/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./lib/config.js";
-export * as files from "./lib/files.js";
-export * from "./sdk/sdk.js";
-//# sourceMappingURL=index.d.ts.map
--- node_modules/@mistralai/mistralai/lib/files.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Consumes a stream and returns a concatenated array buffer. Useful in
- * situations where we need to read the whole file because it forms part of a
- * larger payload containing other fields, and we can't modify the underlying
- * request structure.
- */
-export declare function readableStreamToArrayBuffer(readable: ReadableStream<Uint8Array>): Promise<ArrayBuffer>;
-//# sourceMappingURL=files.d.ts.map
